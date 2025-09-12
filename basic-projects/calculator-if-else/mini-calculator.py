6b34426b25c2885c6c97877b27c6b142b7bb5211
Muhammad Faizan Asim <fizzykhan22865@gmail.com>
2025-09-12 22:21:02 +0500

    Add calculator project (if-else)

---
+++ basic-projects/calculator-if-else/mini-calculator.py
@@ -0,0 +1,52 @@
+# #mini calculator by if-else and elif statement.
+# first=float(input('enter your 1st number:'))
+# operator=input('enter your operator(+,-,*,/,//,%,**):')
+# second=float(input('enter your 2nd number:'))
+
+# if operator=='+' :
+#     print(first+second)
+# elif operator=='-' :
+#     print(first-second) 
+# elif operator=='*' :
+#     print(first*second)
+# elif operator=='/' :
+#     print(first/second)
+# elif operator=='//' :
+#     print(first//second)
+# elif operator=='%' :
+#     print(first%second) 
+# elif operator=='**' :
+#     print(first**second)    
+
+# else:
+#     print('invalid operation')        
+
+
+
+
+# #FUNCTION FOR THIS CALCULATOR
+
+# def calc(first,operator,second):
+    
+    
+#     if operator=='+' :
+#      print(first+second)
+#     elif operator=='-' :
+#         print(first-second) 
+#     elif operator=='*' :
+#         print(first*second)
+#     elif operator=='/' :
+#         print(first/second)
+#     elif operator=='//' :
+#         print(first//second)
+#     elif operator=='%' :
+#         print(first%second) 
+#     elif operator=='**' :
+#         print(first**second)    
+
+#     else:
+#         print('invalid operation')   
+        
+        
+# calc(99,"*",33)     
+
