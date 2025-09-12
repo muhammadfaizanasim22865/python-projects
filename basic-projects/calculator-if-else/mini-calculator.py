9b343d62d670382cbaa4c2c3b6b689053fa02f66
Muhammad Faizan Asim <fizzykhan22865@gmail.com>
2025-09-12 22:29:41 +0500

    Update mini-calculator.py

--- basic-projects/calculator-if-else/mini-calculator.py
+++ basic-projects/calculator-if-else/mini-calculator.py
@@ -1,52 +1,35 @@
-# #mini calculator by if-else and elif statement.
-# first=float(input('enter your 1st number:'))
-# operator=input('enter your operator(+,-,*,/,//,%,**):')
-# second=float(input('enter your 2nd number:'))
+# Mini calculator using if-else and elif statement
 
-# if operator=='+' :
-#     print(first+second)
-# elif operator=='-' :
-#     print(first-second) 
-# elif operator=='*' :
-#     print(first*second)
-# elif operator=='/' :
-#     print(first/second)
-# elif operator=='//' :
-#     print(first//second)
-# elif operator=='%' :
-#     print(first%second) 
-# elif operator=='**' :
-#     print(first**second)    
+first = float(input('Enter your 1st number: '))
+operator = input('Enter your operator (+, -, *, /, //, %, **): ')
+second = float(input('Enter your 2nd number: '))
 
-# else:
-#     print('invalid operation')        
+if operator == '+':
+    print(first + second)
+elif operator == '-':
+    print(first - second)
+elif operator == '*':
+    print(first * second)
+elif operator == '/':
+    if second != 0:
+        print(first / second)
+    else:
+        print("Error: Division by zero is not allowed")
+elif operator == '//':
+    if second != 0:
+        print(first // second)
+    else:
+        print("Error: Floor division by zero is not allowed")
+elif operator == '%':
+    if second != 0:
+        print(first % second)
+    else:
+        print("Error: Modulus by zero is not allowed")
+elif operator == '**':
+    print(first ** second)
+else:
+    print('Invalid operation')
 
 
 
-
-# #FUNCTION FOR THIS CALCULATOR
-
-# def calc(first,operator,second):
-    
-    
-#     if operator=='+' :
-#      print(first+second)
-#     elif operator=='-' :
-#         print(first-second) 
-#     elif operator=='*' :
-#         print(first*second)
-#     elif operator=='/' :
-#         print(first/second)
-#     elif operator=='//' :
-#         print(first//second)
-#     elif operator=='%' :
-#         print(first%second) 
-#     elif operator=='**' :
-#         print(first**second)    
-
-#     else:
-#         print('invalid operation')   
-        
-        
-# calc(99,"*",33)     
 
